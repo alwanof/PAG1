92df8b48745d1a9af1f224ada58bdf08501ac57f
Murad Alwan <free1soft@gmail.com>
2023-02-28 09:15:53 +0300

    test is ready

--- README.md
+++ README.md
@@ -7,5 +7,5 @@ The objective of this assignment is to assess the student's understanding and ab
 1. Declare and initialize 3 variables of different data types: **name(string)**, **age(integer)**, and **isStudent(boolean)**.
 2. Create an indexed array: **numbers**  and an associative array: **person**, each with at least 5 elements.
 3. Write a conditional statement using if-else to check if **age** declared in task 1 is odd or even, Declare **result** variable and assign to it: **"Your age is even"** in case of even and: **"Your age is odd"** in case of odd.
-4. Use a for loop to iterate over each element in the indexed array(**numbers**) created in task 2 , Declare **oddarr** : it's new array should contains each odd element.
+4. Use some kind of loop to iterate over each element in the indexed array(**numbers**) created in task 2 , Declare **oddarr** : it's new array should contains each odd element.
 5. Write a custom function named: **repeatString** that takes two arguments, an integer and a string, and returns the string repeated the number of times specified by the integer.
--- src/PAG1.php
+++ src/PAG1.php
@@ -1,31 +1,41 @@
 <?php
-// Task1: Define variables
-// Define a string variable
+/* Task1: Declare and initialize 3 variables of different data types:
+     name(string), 
+     age(integer), 
+     and isStudent(boolean).*/
+
 $name = null;
 
-// Define a numeric variable
+
 $age = null;
 
-// Define a boolean variable
+
 $isStudent = null;
 
-//task2: Define arrays
-// Define a numeric array
+/*task2: Create an indexed array named (numbers) and an associative array named (person), 
+each with at least 5 elements.*/
+
 $numbers = null;
 
-// Define an associative array
 
 $person = null;
-// task3:
-// check if a age is odd or even
+
+/* task3: Write a conditional statement using if-else to check if age declared in task 1 is odd or even, 
+look (result) variable and assign to it: 
+- "Your age is even" in case of even 
+- "Your age is odd" in case of odd.*/
+
 $result = '';
 // your code here..
 
 
+/* task4: Use some kind of loop to iterate over each element in the indexed array(numbers) created in task 2 , 
+- look (oddarr) : it's new array should contains each odd element.*/
 $oddarr = [];
-// add all odd numbers in [numbers] array to [oddarr] array
 // your code here..
 
-// task4: Define a repeatString
-// repeatString(3, "Hello") returns "HelloHelloHello"
+/* task5:Write a custom function named: 
+(repeatString) that takes 2 arguments, an integer and a string, 
+and returns the string repeated the number of times specified by the integer.*/
+
 // your code here..

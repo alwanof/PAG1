71394e0d0ec1a413521b8ec2d8552c1440aaefa9
Alaa Al Jebbeh <126255179+AlaaAlJebbeh@users.noreply.github.com>
2023-03-08 20:25:12 +0300

    Alaas' solution

--- src/PAG1.php
+++ src/PAG1.php
@@ -4,38 +4,57 @@
      age(integer), 
      and isStudent(boolean).*/
 
-$name = null;
-
-
-$age = null;
-
-
-$isStudent = null;
+$name = "Alaa";
+$age = 23;
+$isStudent = true;
 
 /*task2: Create an indexed array named (numbers) and an associative array named (person), 
 each with at least 5 elements.*/
 
-$numbers = null;
-
-
-$person = null;
+$numbers = [9,6,7,19,2,4,11,17,8];
+$person =[
+        "manager" => "Alaa",
+        "developer" => "Mohamed",
+        "test" => "Mohanad",
+        "designer" => "Azize",
+        "HR" => "Ahmed",
+        "social medis" => "WWP"
+    ];
 
 /* task3: Write a conditional statement using if-else to check if age declared in task 1 is odd or even, 
 look (result) variable and assign to it: 
 - "Your age is even" in case of even 
 - "Your age is odd" in case of odd.*/
 
-$result = '';
-// your code here..
+$result = " ";
 
+if($age % 2 == 0){
+    $result = "Your age is even \n";
+}
+else{
+    $result = "Your age is odd \n";
+}
 
 /* task4: Use some kind of loop to iterate over each element in the indexed array(numbers) created in task 2 , 
 - look (oddarr) : it's new array should contains each odd element.*/
 $oddarr = [];
-// your code here..
-
+$i = 0;
+$j = 0;
+while($i < count($numbers)){
+    if($numbers[$i] % 2 != 0){
+        $oddarr[$j] = $numbers[$i];
+        $j++;
+    }        
+    $i++;
+}
 /* task5:Write a custom function named: 
 (repeatString) that takes 2 arguments, an integer and a string, 
 and returns the string repeated the number of times specified by the integer.*/
 
-// your code here..
+function repeatString($repeat , $statement){
+    for($i = 0; $i < $repeat; $i++){
+        echo $statement . " \n";
+    }
+}
+
+
